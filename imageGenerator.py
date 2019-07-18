784d67baac694b03f77fa6a6c90b7d3b35ef9dbc
Roman <romanbondarenko14@gmail.com>
2019-07-18 12:32:34 +0300

    final image generator commit

--- imageGenerator.py
+++ imageGenerator.py
@@ -9,15 +9,17 @@ def generate_image(time):
     img = Image.new('RGB', (IMAGE_W, IMAGE_H), color=(0, 0, 0))
     font = ImageFont.truetype('./font/digital-7.ttf', TEXT_SIZE)
     d = ImageDraw.Draw(img)
-    text_w, text_h = d.textsize()
-    d.text(((IMAGE_W - text_w) / 2, (IMAGE_H - text_h) / 2), time, font=font, fill=(255, 255, 0))
+    text_w, text_h = d.textsize(time, font)
+    d.text(((IMAGE_W-text_w)/2, (IMAGE_H-text_h)/2), time, font=font, fill=(255, 255, 255))
     img.save('./images/' + time + '.png')
 
 
 def generate_images():
     if not os.path.exists('./images'):
-        print('start generating images...')
         os.mkdir('./images')
+
+    if len(os.listdir('./images')) == 0:
+        print('start generating images...')
         for hour in range(0, 24):
             for minute in range(0, 60):
                 time = '0' + str(hour) + ':' if hour < 10 else str(hour) + ':'
